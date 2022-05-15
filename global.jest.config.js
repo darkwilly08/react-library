2c246aad6cea0cc2262426afd45d2d2cf1c0b0c3
Franco Berardi <darkwilly08@gmail.com>
2022-05-15 12:22:20 -0300

    test(global.jest): jest css parser added

--- global.jest.config.js
+++ global.jest.config.js
@@ -3,13 +3,18 @@ const { getJestProjects } = require('@nrwl/jest');
 module.exports = {
   // projects: getJestProjects(),
   moduleNameMapper: {
-    '\\.(css|less|scss)$': '<rootDir>/config/tests//styleMock.js',
+    '.+\\.(css|scss)$': 'identity-obj-proxy',
     '@darkwilly08/app-bar': '<rootDir>/libs/app-bar/src/index.ts',
     '@darkwilly08/button': '<rootDir>/libs/button/src/index.ts',
     '@darkwilly08/common': '<rootDir>/libs/common/src/index.ts',
     '@darkwilly08/drawer': '<rootDir>/libs/drawer/src/index.ts',
     '@darkwilly08/icon-wrapper': '<rootDir>/libs/icon-wrapper/src/index.ts',
   },
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testMatch: ['<rootDir>/libs/**/tests/*.spec.tsx'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
   testEnvironment: 'jsdom',
 };
