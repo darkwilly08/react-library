6aee4af749d496db783777f7281d2d07737227cb
Franco Berardi <darkwilly08@gmail.com>
2022-05-18 13:04:41 -0300

    WIP(link): new component

---
+++ libs/link/jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'link',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/link'
+};
